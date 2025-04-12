72c8cff1664633135d92da239f954383722e7ede
nisha1126 <nishanthinia1@gmail.com>
2025-04-12 10:13:55 +0530

    search

--- functions/app.js
+++ functions/app.js
@@ -1,32 +1,29 @@
 import "./database.js";
-import { PostModule} from "./models.js";
+import { PostModule } from "./models.js";
 import express from "express";
 
 export const app = express();
 
 app.use(express.json());
 
-  app.get("/api/search", (req, res) => {
-    const text = req.query.text;
-  
-    PostModule1.find({
+app.get("/api/search", async (req, res) => {
+  const text = req.query.text;
+
+  if (!text) {
+    return res.status(400).send({ message: "Search text is required." });
+  }
+
+  try {
+    const results = await PostModule.find({
       $or: [
-        {
-          title: new RegExp(text, "i"),
-        },
-        {
-          content: new RegExp(text, "i"),
-        },
+        { title: new RegExp(text, "i") },
+        { content: new RegExp(text, "i") },
       ],
-    })
-      .then((results) => {
-        res.send(results);
-      })
-      .catch((error) => {
-        console.error("Oops!Failed to search...", error);
-  
-        res.status(500).send({
-          message: "Oops!Failed to search...",
-        });
-      });
-  })
+    });
+
+    res.send(results);
+  } catch (error) {
+    console.error("Oops! Failed to search...", error);
+    res.status(500).send({ message: "Oops! Failed to search..." });
+  }
+});
